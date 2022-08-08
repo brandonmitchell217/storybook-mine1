e7139c9a1b08a9f70e5581bcbc73666239a9b3df
Brandon Mitchell <brandonmitchell217@gmail.com>
2022-08-08 12:28:57 -0400

    Added card stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Button } from "./components/Button/Button";
+import { Card } from "./components/Card/Card";
 
 function App() {
+  const desc =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo at eum obcaecati itaque minima, ad vitae iste, asperiores, quaerat rem sint aliquam repellendus pariatur cum.";
   return (
     <div className="App">
       <h1>hey</h1>
       <Button text="The Button" primary size="large" />
+      <Card
+        title="Stuff Happened"
+        description={desc}
+        image="https://images.unsplash.com/photo-1659944975073-453265ccf3a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+      />
     </div>
   );
 }
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "../Button/Button";
 
 export interface CardProps {
   image: string;
@@ -7,10 +8,21 @@ export interface CardProps {
 }
 export const Card = ({ image, title, description, ...props }: CardProps) => {
   return (
-    <div style={{ width: "350px" }} {...props}>
+    <div
+      style={{
+        width: "350px",
+        margin: "1rem 0",
+        padding: ".5rem",
+        border: "1px solid rgba(0, 0, 0, 0.5)",
+        borderRadius: "10px",
+        boxShadow: "0 2px 8px rgba(0, 0, 0, 0.2)",
+      }}
+      {...props}
+    >
       <img src={image} alt="" style={{ display: "block", maxWidth: "100%" }} />
       <h1>{title}</h1>
-      <p>{description}</p>
+      <p style={{ marginBottom: "12px" }}>{description}</p>
+      <Button text="The Button" size="small" />
     </div>
   );
 };
