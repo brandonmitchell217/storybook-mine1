36e890bcc60c8bfd12fc7fc2593da2414c4f7446
Brandon Mitchell <brandonmitchell217@gmail.com>
2022-08-08 21:42:44 -0400

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import { Card } from "./components/Card/Card";
 import { data } from "./data";
 
 function App() {
-  // const desc =
-  //   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo at eum obcaecati itaque minima, ad vitae iste, asperiores, quaerat rem sint aliquam repellendus pariatur cum.";
   return (
     <div
       className="App"
@@ -17,12 +15,6 @@ function App() {
     >
       <h1>hey</h1>
       <Button text="The Button" primary size="large" />
-      {/* <Card
-        primary
-        title="Stuff Happened"
-        description={desc}
-        image="https://images.unsplash.com/photo-1659944975073-453265ccf3a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-      /> */}
       <div
         css={css({
           display: "flex",
@@ -30,12 +22,12 @@ function App() {
           alignItems: "center",
         })}
       >
-        {data.map((thing) => (
+        {data.map((item) => (
           <Card
             primary
-            title={thing.title}
-            description={thing.description}
-            image={thing.image}
+            title={item.title}
+            description={item.description}
+            image={item.image}
           />
         ))}
       </div>
