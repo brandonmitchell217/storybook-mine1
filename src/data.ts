b18849830015b674e9196c5c86676013b6a00003
Brandon Mitchell <brandonmitchell217@gmail.com>
2022-08-08 22:08:13 -0400

    Added in a few more data objects

--- src/data.ts
+++ src/data.ts
@@ -15,4 +15,20 @@ export const data = [
     image:
       "https://images.unsplash.com/photo-1659974753898-a13d36032ba1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2200&q=80",
   },
+  {
+    id: 3,
+    title: "A Thing 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates a esse aliquam laudantium consequuntur error harum voluptatum dolores.",
+    image:
+      "https://images.unsplash.com/photo-1659948782691-369ce33c44d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  },
+  {
+    id: 4,
+    title: "A Thing 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates a esse aliquam laudantium consequuntur error harum voluptatum dolores.",
+    image:
+      "https://images.unsplash.com/photo-1659974753898-a13d36032ba1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2200&q=80",
+  },
 ];
